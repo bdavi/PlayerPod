229db3e00c0cd812d3efb646cfb4a1b9266bfc6c
Brian Davies <brian@briandavies.me>
2015-08-13 19:45:10 -0600

    Turn off email confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # devise :database_authenticatable, :registerable, :confirmable,
+  #        :recoverable, :rememberable, :trackable, :validatable
+
+  devise :database_authenticatable, :registerable, :recoverable, 
+         :rememberable, :trackable, :validatable
 
   has_many :subscriptions
   has_many :feeds, through: :subscriptions
