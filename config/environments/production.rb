96bdeee6d1ce67f5b4724ac6b65503fba1421aa0
Brian Davies <brian@briandavies.me>
2015-08-13 19:40:17 -0600

    Set default mail for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Set default mailer URL
+  config.action_mailer.default_url_options = { host: 'radiant-ocean-9425.herokuapp.com', port: 3000 } 
+
 end
