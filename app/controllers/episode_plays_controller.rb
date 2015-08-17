1381243dc26a5d3dffcdea4add8f40aa8f1d2b98
Brian Davies <brian@briandavies.me>
2015-08-16 20:45:31 -0600

    Mark episodes played

--- app/controllers/episode_plays_controller.rb
+++ app/controllers/episode_plays_controller.rb
@@ -1,5 +1,12 @@
 class EpisodePlaysController < ApplicationController
   def create
+    raise Pundit::NotAuthorizedError unless current_user
+
+    EpisodePlay.create(user_id: current_user.id, episode_id: params[:episode_id])
+
+    respond_to do |format|
+      format.js
+    end
   end
 
   def destroy
