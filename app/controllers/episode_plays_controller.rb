246c6197eec24ebf4f0b0e8e1fdf1c2df5d6ea73
Brian Davies <brian@briandavies.me>
2015-08-17 20:35:40 -0600

    First draft of allow un-playing.

--- app/controllers/episode_plays_controller.rb
+++ app/controllers/episode_plays_controller.rb
@@ -9,6 +9,14 @@ class EpisodePlaysController < ApplicationController
     end
   end
 
-  def destroy
+  def destroy_by_episode_and_user
+    raise Pundit::NotAuthorizedError unless current_user
+
+    episode_play = EpisodePlay.find_by(user_id: current_user.id, episode_id: params[:episode_id])
+    episode_play.destroy if episode_play
+
+    respond_to do |format|
+      format.js
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  resources :episode_plays, only: [:create, :destroy]
+  resources :episode_plays, only: [:create]
   resources :episodes, only: [:index]
   resources :subscriptions, only: [:index, :create, :destroy]
 
+  delete 'unplay' => 'episode_plays#destroy_by_episode_and_user'
   devise_for :users
   get 'home' => 'static_pages#home'
   root to: 'static_pages#home'
